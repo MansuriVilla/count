633285882bed01712fdacd7da48a6a010c6ff151
Mansuri Villa <113539390+MansuriVilla@users.noreply.github.com>
2024-07-15 19:00:59 +0530

    Add all file

---
+++ server.js
@@ -0,0 +1,34 @@
+var initialCount = 314;
+var finalCount = 14;
+var duration = 10 * 60 * 1000;
+
+function startCountdown() {
+  var storedStartTime = localStorage.getItem("startTime");
+  var startTime;
+  if (storedStartTime) {
+    startTime = parseInt(storedStartTime, 10);
+  } else {
+    startTime = Date.now();
+    localStorage.setItem("startTime", startTime);
+  }
+
+  var decrementAmount = (initialCount - finalCount) / duration;
+
+  function updateCounter() {
+    var elapsedTime = Date.now() - startTime;
+    var currentCount = initialCount - Math.round(elapsedTime * decrementAmount);
+
+    if (currentCount >= finalCount) {
+      document.getElementById("counter").textContent = currentCount;
+      setTimeout(updateCounter, 1000);
+    } else {
+      document.getElementById("counter").textContent = finalCount;
+      localStorage.setItem("startTime", Date.now());
+      startCountdown();
+    }
+  }
+
+  updateCounter();
+}
+
+startCountdown();
